1f5528c024f0d55a96de872311b64bb606014ea7
Josué Romero <79048209+josuerom@users.noreply.github.com>
2022-05-05 14:10:13 -0500

    Add files via upload

---
+++ config/qtile/settings/path.py
@@ -0,0 +1,8 @@
+# Modificador por Josué Romero
+# Creado po Antonio Sarosi
+# https://twitter.com/josueromr
+# https://github.com/josuerom/arch-linux
+
+from os import path
+
+qtile_path = path.join(path.expanduser('~'), ".config", "qtile")
