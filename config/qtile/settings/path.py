0f317388c66c2f7a58800df7463725d0db3b742d
Josué Romero <79048209+josuerom@users.noreply.github.com>
2022-08-18 19:16:24 -0500

    Delete config/qtile directory

--- config/qtile/settings/path.py
+++
@@ -1,8 +0,0 @@
-# Modificado por Josué Romero
-# Creado por Antonio Sarosi
-# https://twitter.com/josueromr
-# https://github.com/josuerom/install-archlinux
-
-from os import path
-
-qtile_path = path.join(path.expanduser('~'), ".config", "qtile")
