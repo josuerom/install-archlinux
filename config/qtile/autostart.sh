664a13da7d57bbd1f9ee1da9df8ba38a21423f7c
Josué Romero <79048209+josuerom@users.noreply.github.com>
2022-08-18 19:21:27 -0500

    Ajustes mejorados para Qtile

--- config/qtile/autostart.sh
+++ config/qtile/autostart.sh
@@ -3,7 +3,6 @@ picom &
 
 # Ajuste de pantalla por HDMI
 hdmi=`xrandr | grep ' connected' | grep 'HDMI' | awk '{print $1}'`
-
 # Configuración adicional para la pantalla
 if [ "$hdmi" = "HDMI-1" ]; then
   xrandr --output eDP-1 --primary --mode 1366x768 --pos 276x1080 --output HDMI-1 --mode 1920x1080 --pos 0x0 &
@@ -15,19 +14,14 @@ fi
 nm-applet &
 volumeicon &
 cbatticon &
-
 # Distribución del teclado
 setxkbmap latam &
-
 # Dispositivos Automático
 udiskie -t &
-
 # Fuentes Java
 xsettingsd &
-
 # Fondo de pantalla
 nitrogen --restore &
-
 # Barra superpuesta
 xob-pulse-py | xob -s pulse &
 xob-brightness-js | xob -s brightness &
